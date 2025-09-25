09056ab8a832b3c0c74f1a235f5c93ddf8618daa
Troels Marienlund <tmni77103.edu.ucl.dk>
2025-09-25 14:19:47 +0200

    1. udkast program

--- Hydac/Hydac/Program.cs
+++ Hydac/Hydac/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Hydac
+﻿using System.Xml.Linq;
+
+namespace Hydac
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            Employe[] employe = new Employe[]
+            {
+            new Employe { id = 1, name = "Anders Andersen" },
+            new Employe { id = 2, name = "Birgitte Jensen" },
+            new Employe { id = 3, name = "Carsten Nielsen" }
+            };
+            
+            
+        
+            Console.WriteLine("Velkommen");
+            Console.WriteLine("Tast 1 Check ind");
+            Console.WriteLine("Tast 2 Check ud");
+            
+            int InOut = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            if (InOut == 1)
+            {
+                Console.WriteLine("Tast 1 for Medarbejder");
+                Console.WriteLine("Tast 2 for Gæst");
+                
+                int EmployeGuest = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (EmployeGuest == 1)
+                {
+                    Console.Write("Tast medarbejder ID: ");
+                    int id = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
